a42dfd2e143478309883f1b3c5219cd17630fbc1
AymaneElHilali <aaws1234gta@gmail.com>
2024-12-14 02:41:11 +0100

    v2

--- src/main/java/com/elhilali/authentification/controller/UserController.java
+++ src/main/java/com/elhilali/authentification/controller/UserController.java
@@ -1,8 +1,6 @@
 package com.elhilali.authentification.controller;
-
 import com.elhilali.authentification.dataAcces.User;
 import com.elhilali.authentification.service.UserService;
-import com.elhilali.authentification.service.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,38 +13,29 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @Autowired
-    JwtService jwtService;  // Inject JwtService to generate JWT tokens
 
-    // Signup endpoint
     @PostMapping("/signup")
     public User signup(@RequestBody User user){
-        System.out.println(user.getEmail());
         return userService.signup(user);
     }
 
-    // Login endpoint to generate JWT token
+
+
     @PostMapping("/login")
     public String login(@RequestBody User user) {
-        // Check if the admin exists and credentials are correct
-        User foundUser = userService.findByEmail(user.getEmail());
 
-        if (foundUser != null && foundUser.getPassword().equals(user.getPassword())) {
-            // If credentials are valid, generate JWT token
-            String token = jwtService.generateToken(user.getEmail());
-            return "Bearer " + token; // Return token in Bearer format
-        } else {
-            return "Invalid credentials"; // If credentials are invalid
-        }
+        return userService.login(user);
+
     }
 
-    // Protected landing page (requires valid JWT token)
+
+
     @GetMapping("/landingPage")
     public String landingPage(){
         return "pass";
     }
 
-    // Another protected landing page
+
     @GetMapping("/landingPage2")
     public String landingPage2(){
         return "pass";
--- src/main/java/com/elhilali/authentification/service/UserService.java
+++ src/main/java/com/elhilali/authentification/service/UserService.java
@@ -7,14 +7,29 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Autowired
+    JwtService jwtService;
 
     @Autowired
     UserRepo userRepo;
+
     public User signup(User user){
         return userRepo.save(user);
-
     }
+
     public User findByEmail(String email) {
         return userRepo.findByEmail(email);
+    }
+
+    public String login(User user){
+
+        User foundUser = this.findByEmail(user.getEmail());
+        if (foundUser != null && foundUser.getPassword().equals(user.getPassword())) {
+            String token = jwtService.generateToken(user.getEmail());
+            return "Bearer " + token;
+        } else {
+            return "Invalid credentials";
+        }
+
     }
 }
